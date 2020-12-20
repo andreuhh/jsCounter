4b9c5055809458b90f1ed2152613912e4c2394c9
Andrea Guffi <andreaguffi92@gmail.com>
2020-12-20 08:11:56 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,99 @@
+const loveMe = document.querySelector('.loveMe')
+const times = document.querySelector('#times')
+const likeBtn = document.querySelector('.btn-increment')
+const dislikeBtn = document.querySelector('.btn-decrement')
+
+let clickTime = 0
+let timesClicked = 0
+
+
+
+loveMe.addEventListener('click', (e) => {
+    if(clickTime === 0) {
+        clickTime = new Date().getTime()
+    } else {
+        if((new Date().getTime() - clickTime) < 800) {
+            createHeart(e)
+            clickTime = 0
+        } else {
+            clickTime = new Date().getTime()
+        }
+    }
+})
+
+const createHeart = (e) => {
+    const heart = document.createElement('i')
+    heart.classList.add('fas')
+    heart.classList.add('fa-heart')
+
+    const x = e.clientX
+    const y = e.clientY
+
+    const leftOffset = e.target.offsetLeft
+    const topOffset = e.target.offsetTop
+
+    const xInside = x - leftOffset
+    const yInside = y - topOffset
+
+    heart.style.top = `${yInside}px`
+    heart.style.left = `${xInside}px`
+
+    loveMe.appendChild(heart)
+
+    times.innerHTML = ++timesClicked
+
+    setTimeout(() => heart.remove(), 1000)
+}
+
+const createDislike = (e) => {
+    const heart = document.createElement('i')
+    heart.classList.add('fas')
+    heart.classList.add('fa-thumbs-down')
+
+    const x = e.clientX 
+    const y = e.clientY 
+
+    const leftOffset = e.target.offsetLeft
+    const topOffset = e.target.offsetTop
+
+    const xInside = x - leftOffset + Math.floor(Math.random() * 100);
+    const yInside = y - topOffset +  Math.floor(Math.random() * 100);
+
+    heart.style.top = `${yInside}px`
+    heart.style.left = `${xInside}px`
+
+    loveMe.appendChild(heart)
+
+    times.innerHTML = --timesClicked
+
+    setTimeout(() => heart.remove(), 1000)
+}
+
+
+const createRandomHeart = (e) => {
+    
+    const heart = document.createElement('i')
+    heart.classList.add('fas')
+    heart.classList.add('fa-heart')
+
+    const x = e.clientX
+    const y = e.clientY
+
+    const leftOffset = e.target.offsetLeft
+    const topOffset = e.target.offsetTop
+
+    const xInside = x - leftOffset + Math.floor(Math.random() * 100);
+    const yInside = y - topOffset +  Math.floor(Math.random() * 100);
+
+    heart.style.top = `${yInside}px`
+    heart.style.left = `${xInside}px`
+
+    loveMe.appendChild(heart)
+
+    times.innerHTML = ++timesClicked
+
+    setTimeout(() => heart.remove(), 1000)
+}
+
+likeBtn.addEventListener('click', createRandomHeart )
+dislikeBtn.addEventListener('click', createDislike )
